79acb63445b7060dd6ed671b6d01ab774b155ae0
Rishav Kumar <developerrishav69@gmail.com>
2025-11-12 15:00:30 +0530

    Update PlaceOrder.jsx

--- Frontend/src/Pages/PlaceOrder.jsx
+++ Frontend/src/Pages/PlaceOrder.jsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import CartTotal from "../component/CartTotal";
 import Razorpay from "../assets/Razorpay.jpg";
 import { shopDataContext } from "../context/ShopContext";
-import { authDataContext } from "../context/authContext";
+import { authDataContext } from "../context/AuthContext";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
  import { toast } from 'react-toastify';
