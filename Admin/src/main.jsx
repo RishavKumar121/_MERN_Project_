bb41cc4795a9401a6274ba1b3724626bc7747c55
Rishav Kumar <developerrishav69@gmail.com>
2025-11-12 15:21:00 +0530

    Update main.jsx

--- Admin/src/main.jsx
+++ Admin/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import AuthContext from "./context/authContext.jsx";
+import AuthContext from "./context/AuthContext.jsx";
 
 //for routing
 import { BrowserRouter } from "react-router-dom";
