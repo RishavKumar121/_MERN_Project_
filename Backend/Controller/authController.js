9c696107882d35c5fa4b059103867771c03d1e7f
Rishav Kumar <developerrishav69@gmail.com>
2025-11-13 10:27:08 +0530

    Update authController.js

--- Backend/Controller/authController.js
+++ Backend/Controller/authController.js
@@ -65,15 +65,19 @@ export const login = async(req, res)=>{
   }
 }
 
-export const logout = async(req, res)=>{
-try {
-   res.clearCookie("token")
-   return res.status(200).send({message:"Logout Successfully !!!"})
-} catch (error) {
-    console.log(error)
-      res.status(404).send({message:"Logout error occcured..."})
-}
-}
+export const logout = async (req, res) => {
+  try {
+    res.clearCookie("token", {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+    });
+    return res.status(200).send({ message: "Logout Successfully !!!" });
+  } catch (error) {
+    console.log(error);
+    res.status(404).send({ message: "Logout error occurred..." });
+  }
+};
 
 
 export const adminLogin =async (req, res)=>{
